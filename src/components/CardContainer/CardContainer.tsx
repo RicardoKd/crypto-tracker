9ea8f1ea2cdb8f281b9c4ce2aff85f6630b69580
RicardoKd <richardo.kadyan@gmail.com>
2022-08-05 19:18:18 +0300

    v1.2.0 drag n drop added for CardContainer children

--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,21 +1,66 @@
 import React from "react";
-import { useAppSelector } from "../../app/hooks";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
 
-import { selectRenderedCoins } from "../../app/coinSlice";
+import { ReactSortable, ItemInterface } from "react-sortablejs";
+
+import { selectRenderedCoins, setCoinsToRender } from "../../app/coinSlice";
 
 import styles from "./CardContainer.module.css";
 
 const CardContainer = () => {
   const renderedCoins = useAppSelector(selectRenderedCoins);
 
-  
+  const dispatch = useAppDispatch();
+
+  const checkListIdenticaltoRenderedCoins = (list: string[]) : boolean => {
+    let i = 0;
+    let areIdentical = true;
+
+    list.forEach((coinName: string) => {
+      if (renderedCoins[i] !== coinName) {
+        areIdentical = false;
+      }
+
+      i++;
+    });
+
+    return areIdentical;
+  };
+
+  const reactSortableSetList = (newList: ItemInterface[]) : void => {
+    if (!newList.length) {
+      return;
+    }
+    const newListFormated = newList.map(
+      (el) => el.coinName,
+    );
+
+    if (checkListIdenticaltoRenderedCoins(newListFormated)) {
+      return;
+    }
+
+    dispatch(setCoinsToRender(newListFormated));
+  };
+
+  const reactSortableGetList = renderedCoins.map((coinName) : ItemInterface => {
+    return { coinName, id: Math.random() };
+  });
 
   return (
-<div className={styles.cardContainer}>
+    <ReactSortable
+      className={styles.cardContainer}
+      list={reactSortableGetList}
+      setList={(newList: ItemInterface[]) => reactSortableSetList(newList)}
+      animation={500}
+      delayOnTouchOnly={true}
+      delay={500}
+    >
       {renderedCoins.map((coinName, index) => (
+        // <TiltCoinCard coinName={coinName} key={index} />
         <p key={index}>{coinName}</p>
       ))}
-</div>
+      {/* <TiltCoinCardCreatorCard /> */}
+    </ReactSortable>
   );
 };
 
